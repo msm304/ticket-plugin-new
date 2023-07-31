b44e606b4be5e2973ea3f77613a9740561ec519d
msm304 <soltani5747@gmail.com>
2023-07-31 16:07:55 +0330

    edit ticket in admin side

--- inc/admin/tkt-menu.php
+++ inc/admin/tkt-menu.php
@@ -8,6 +8,7 @@ class TKT_Menu extends Base_Menu
 
     private $wpdb;
     private $table;
+    private $reply_table;
     private $ticket_id = null;
 
     public function __construct()
@@ -16,6 +17,7 @@ class TKT_Menu extends Base_Menu
         global $wpdb;
         $this->wpdb = $wpdb;
         $this->table = $wpdb->prefix . 'tkt_tickets';
+        $this->reply_table = $wpdb->prefix . 'tkt_replies';
 
         $this->page_title = 'تیکت پشتیبانی';
         $this->menu_title = 'تیکت پشتیبانی';
@@ -76,14 +78,17 @@ class TKT_Menu extends Base_Menu
 
         parent::__construct();
     }
+
     public function page()
     {
         echo 'ticket plugin page';
     }
+
     public function tickets_page()
     {
         include TKT_VIEWS_PATH . 'admin/ticket/main.php';
     }
+
     public function tickets_screen_option()
     {
         // add screen option
@@ -96,6 +101,7 @@ class TKT_Menu extends Base_Menu
 
         $this->ticket_object = new TKT_Ticket_list();
     }
+
     public function new_ticket_page()
     {
         $is_edit = false;
@@ -117,6 +123,7 @@ class TKT_Menu extends Base_Menu
         }
         include TKT_VIEWS_PATH . 'admin/ticket/new.php';
     }
+
     public function create_ticket($creator_id, $data)
     {
         // create ticket
@@ -145,9 +152,49 @@ class TKT_Menu extends Base_Menu
         }
         return $ids;
     }
+
     public function edit_ticket_page()
     {
         $is_edit = true;
+
+        $currnet_user = get_current_user_id();
+
+        if (isset($_POST['publish'])) {
+
+            if (!isset($_POST['ticket_nonce']) || !wp_verify_nonce($_POST['ticket_nonce'], 'ticket_security')) {
+                echo 'Sorry, nonce not verify';
+                exit;
+            }
+
+            // update ticket
+            $data = $_POST;
+            $update = $this->update_ticket($data);
+
+            // reply insert
+
+            $insert_reply = 0;
+            if (isset($data['tkt-reply-content']) && !empty($data['tkt-reply-content'])) {
+
+                $reply_data = [
+                    'ticket_id' => $this->ticket_id,
+                    'creator_id' => $currnet_user,
+                    'from_admin' => 1,
+                    'body' => stripslashes_deep($data['tkt-reply-content']),
+                    'file' => isset($data['reply-file']) ? sanitize_text_field($data['reply-file']) : null,
+                ];
+                $insert_reply = $this->create_reply($reply_data);
+                if ($insert_reply) {
+                    $this->update_reply_date();
+                }
+            }
+
+            // replies update
+
+            if ($update || $insert_reply) {
+                TKT_Flash_Message::add_message('تیکت با موفقیت آپدیت شد');
+            }
+        }
+
         $ticket = $this->get_ticket();
 
         $reply_manager = new TKT_Reply_Manager($this->ticket_id);
@@ -155,6 +202,44 @@ class TKT_Menu extends Base_Menu
 
         include TKT_VIEWS_PATH . 'admin/ticket/new.php';
     }
+
+    public function create_reply($data)
+    {
+        $insert = $this->wpdb->insert(
+            $this->reply_table,
+            $data,
+            ['%d', '%d', '%d', '%s', '%s'],
+        );
+
+        return $insert ? $this->wpdb->insert_id : null;
+    }
+
+    public function update_reply_date()
+    {
+        return $this->wpdb->query($this->wpdb->prepare("UPDATE " . $this->table . " SET reply_date = NOW() WHERE ID = %d", $this->ticket_id));
+    }
+
+    public function update_ticket($data)
+    {
+        return $this->wpdb->update(
+            $this->table,
+            [
+                'title' => sanitize_text_field($data['title']),
+                'body' => stripslashes_deep($data['tkt-content']),
+                'creator_id' => isset($data['creator-id']) ? intval($data['creator-id']) : null,
+                'user_id' => isset($data['user-id']) ? intval($data['user-id']) : null,
+                'department_id' => $data['department-id'],
+                'status' => $data['status'],
+                'priority' => $data['priority'],
+                'create_date' => sanitize_text_field($data['create-date']),
+                'file' => $data['file'] ? sanitize_text_field($data['file']) : null
+            ],
+            ['ID' => $this->ticket_id],
+            ['%s', '%s', '%d', '%d', '%d', '%s', '%s', '%s', '%s'],
+            ['%d']
+        );
+    }
+
     public function get_ticket()
     {
         if (!intval($this->ticket_id)) {
